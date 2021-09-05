669ef8dfc4b9cff71f84c58f15885adc71996f4e
sajiburrahmansajib <mrahman172152@bscse.uiu.ac.bd>
2021-09-05 20:45:42 +0600

    Simple java program Created

---
+++ src/simple_java_program.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class simple_java_program {
+    public static void main(String[] args) {
+        System.out.println("Hello Munna , We need to create a project");
+    }
+}
