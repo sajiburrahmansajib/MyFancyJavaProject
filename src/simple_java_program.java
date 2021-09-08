22cb20b6047ea63f70dc4cf8ca6b15db05ac4d7e
Munna Ahamed <mahamed172146@bscse.uiu.ac.bd>
2021-09-08 22:10:41 +0600

    Singleton Pattern implement

--- src/simple_java_program.java
+++ src/simple_java_program.java
@@ -1,5 +1,15 @@
 import javax.swing.*;
 
+class SingletonEagar {
+  private static SingletonEagar instance = new SingletonEagar(); 
+  
+  private SingletonEagar(){}
+  
+  public static SingletonEagar getInstance() {
+    return instance;
+  }
+}
+
 public class simple_java_program {
     public static void main(String[] args) {
         System.out.println("Hello Munna , We need to create a project");
