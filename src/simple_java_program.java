a6602377c680fe8e1a74722e64296b224a22ce2a
sajiburrahmansajib <mrahman172152@bscse.uiu.ac.bd>
2021-09-08 22:21:08 +0600

    Change my code

--- src/simple_java_program.java
+++ src/simple_java_program.java
@@ -1,4 +1,47 @@
 import javax.swing.*;
+class Singleton {
+  private static Singleton instance; 
+  
+  private Singleton(){}
+  
+  public static Singleton getInstance() {
+    if(instance == null) {
+      instance = new Singleton();
+    }
+    
+    return instance;
+  }
+}
+
+class SingletonSynchronizedMethod {
+  private static SingletonSynchronizedMethod instance; 
+  
+  private SingletonSynchronizedMethod(){}
+  
+  public static synchronized SingletonSynchronizedMethod getInstance() {
+    if(instance == null) {
+      instance = new SingletonSynchronizedMethod();
+    }
+    return instance;
+  }
+}
+
+class SingletonSynchronized {
+  private static SingletonSynchronized instance; 
+  
+  private SingletonSynchronized(){}
+  
+  public static SingletonSynchronized getInstance() {
+    if(instance == null) {
+      synchronized (SingletonSynchronized.class) {
+        if(instance == null) {
+          instance = new SingletonSynchronized();
+        }
+      }
+    }
+    return instance;
+  }
+}
 
 public class simple_java_program {
     public static void main(String[] args) {
